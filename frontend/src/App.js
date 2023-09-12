2efa4bc58c14b5f4701aebb8ae120f504cd7225e
theblessed-coder <blessingnwakwuo8@gmail.com>
2023-09-12 12:56:33 +0100

    created folder structure

---
+++ frontend/src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      Casa Quest Hub
+      </header>
+      
+      <main>
+
+      </main>
+
+      <footer>
+        All rights reserved.
+      </footer>
+    </div>
+  );
+}
+
+export default App;
