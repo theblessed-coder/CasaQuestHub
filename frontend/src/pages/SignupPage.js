a8662b4b61ae9aa1e9a32d1ac2cd3c461404223b
theblessed-coder <blessingnwakwuo8@gmail.com>
2023-09-12 17:30:27 +0100

    added more content on the signup page

--- frontend/src/pages/SignupPage.js
+++ frontend/src/pages/SignupPage.js
@@ -16,56 +16,82 @@ const SignupPage = () => {
 
 
     return (
-        <div>
-            <h2>Sign Up</h2>
-            <form onSubmit={handleSubmit}>
-                <div>
-                    <label>First Name:</label>
-                    <input 
-                        type="text"
-                        name="firstName"
-                        value={formData.firstName}
-                        onChange={handleChange}
-                        required
-                    />
-                </div>
+      <div>
+        <h2>Sign Up</h2>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label>First Name:</label>
+            <input
+              type="text"
+              name="firstName"
+              value={formData.firstName}
+              onChange={handleChange}
+              required
+            />
+          </div>
 
-                <div>
-                    <label>Last Name:</label>
-                    <input 
-                        type="text"
-                        name="lastName"
-                        value={formData.lastName}
-                        onChange={handleChange}
-                        required
-                    />
-                </div>
+          <div>
+            <label>Last Name:</label>
+            <input
+              type="text"
+              name="lastName"
+              value={formData.lastName}
+              onChange={handleChange}
+              required
+            />
+          </div>
 
-                <div>
-                    <label>Email:</label>
-                    <input 
-                        type="email"
-                        name="email"
-                        value={formData.email}
-                        onChange={handleChange}
-                        required
-                    />
-                </div>
+          <div>
+            <label>Email:</label>
+            <input
+              type="email"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+              required
+            />
+          </div>
 
-                <div>
-                    <label>Password:</label>
-                    <input
-                        type="password"
-                        name="password"
-                        value={formData.password}
-                        onChange={handleChange}
-                        required
-                    />
-                </div>
+          <div>
+            <label>Password:</label>
+            <input
+              type="password"
+              name="password"
+              value={formData.password}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <p>
+            By clicking Register Now, you agree to CasaQuestHub's Privacy
+            Policy, Terms and Conditions.
+          </p>
+          <button type="submit">REGISTER NOW</button>
+          <p>Already have an account? </p>
+        </form>
 
-                <button type="submit">Sign Up</button>
-            </form>
-        </div>
+        <footer>
+          <div>
+            <ul>
+              <li>
+                <h2>Quick Links</h2>
+              </li>
+              <li>Home</li>
+              <li>About Us</li>
+              <li>Contact</li>
+            </ul>
+          </div>
+
+          <div>
+            <ul>
+              <li>Follow Us</li>
+            </ul>
+          </div>
+
+          <hr></hr>
+          <p>CasaQuestHub &copy; 2023. All rights reserved</p>
+        </footer>
+      </div>
     );
 };
 
