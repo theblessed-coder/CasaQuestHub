335858ed8a6b2c5456de82c35391ef8d56d4fa13
theblessed-coder <blessingnwakwuo8@gmail.com>
2023-09-15 20:29:07 +0100

    added transition on submit button

--- frontend/src/pages/SignupPage.js
+++ frontend/src/pages/SignupPage.js
@@ -1,23 +1,31 @@
 import { useState } from "react";
 
 const SignupPage = () => {
-    const [formData, setFormData] = useState({firstName: '', lastName: '', email: '', password: ''});
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+  });
 
-    const handleChange = (e) =>  {
-        const { name, value } = e.target;
-       setFormData({ ...formData, [name]: value}); 
-    };
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        //Add code to handle signup and send data to the backend
-        console.log('Form submitted:', formData)
-    };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    //Add code to handle signup and send data to the backend
+    console.log("Form submitted:", formData);
+  };
 
-
-    return (
-      <div className="max-w-lg mx-auto my-5 bg-white rounded p-8 shadow-lg flex flex-col">
-        <form onSubmit={handleSubmit}>
+  return (
+    <div className="max-w-lg mx-auto my-5 bg-white rounded p-8 shadow-lg flex flex-col">
+      <>
+        <form
+          className="transition duration-1000 ease-in-out"
+          onSubmit={handleSubmit}
+        >
           <div className="mb-4">
             <label className="block font-semibold mb-2">First Name</label>
             <input
@@ -65,20 +73,23 @@ const SignupPage = () => {
               required
             />
           </div>
-          <p>
-            By clicking Register Now, you agree to CasaQuestHub's Privacy
-            Policy, Terms and Conditions.
+          <p className="text-center pb-2">
+            By clicking Register Now, you agree to CasaQuestHub's <br />
+            Privacy Policy, Terms and Conditions.
           </p>
           <button
             type="submit"
-            className="bg-yellow-500 hover:bg-slate-500 hover:text-yellow-50 text-black font-semibold w-full p-2 rounded transition duration-500 ease-in-out"
+            className="bg-yellow-500 hover:bg-slate-500 hover:text-yellow-50 transform hover:translate-y-[-5px] text-black font-semibold w-full p-2 rounded transition duration-500 ease-in-out"
           >
             REGISTER NOW
           </button>
-          <p>Already have an account? </p>
+          <p className="text-right pt-2">Already have an account? </p>
         </form>
-      </div>
-    );
+      </>
+      {/*
+      <footer className="bg">&copy; 2023 CasaQuestHub</footer> */}
+    </div>
+  );
 };
 
 export default SignupPage;
