6e6e2b4dff3e7c9edd040c1147a748fef9863164
theblessed-coder <blessingnwakwuo8@gmail.com>
2023-09-15 18:45:53 +0100

    changed form button color

--- frontend/src/pages/SignupPage.js
+++ frontend/src/pages/SignupPage.js
@@ -17,48 +17,51 @@ const SignupPage = () => {
 
     return (
       <div className="max-w-md mx-auto bg-white rounded p-8 shadow-lg">
-        <h2 className="text-xl font-bold mb-6">Sign Up</h2>
         <form onSubmit={handleSubmit}>
           <div className="mb-4">
-            <label>First Name:</label>
+            <label className="block font-semibold mb-2">First Name:</label>
             <input
               type="text"
               name="firstName"
               value={formData.firstName}
               onChange={handleChange}
+              className="w-full p-2 border border-gray-300 rounded"
               required
             />
           </div>
 
-          <div>
-            <label>Last Name:</label>
+          <div className="mb-4">
+            <label className="block font-semibold mb-2">Last Name:</label>
             <input
               type="text"
               name="lastName"
               value={formData.lastName}
               onChange={handleChange}
+              className="w-full p-2 border border-gray-300 rounded"
               required
             />
           </div>
 
-          <div>
-            <label>Email:</label>
+          <div className="mb-4">
+            <label className="block font-semibold mb-2">Email:</label>
             <input
               type="email"
               name="email"
               value={formData.email}
               onChange={handleChange}
+              className="w-full p-2 border border-gray-300 rounded"
               required
             />
           </div>
 
-          <div>
-            <label>Password:</label>
+          <div className="mb-6">
+            <label className="block font-semibold mb-2">Password:</label>
             <input
               type="password"
               name="password"
               value={formData.password}
               onChange={handleChange}
+              className="w-full p-2 border border-gray-300 rounded"
               required
             />
           </div>
@@ -66,7 +69,12 @@ const SignupPage = () => {
             By clicking Register Now, you agree to CasaQuestHub's Privacy
             Policy, Terms and Conditions.
           </p>
-          <button type="submit">REGISTER NOW</button>
+          <button
+            type="submit"
+            className="bg-yellow-500 text-black font-semibold w-full p-2 rounded"
+          >
+            REGISTER NOW
+          </button>
           <p>Already have an account? </p>
         </form>
       </div>
