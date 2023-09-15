6b8c2c14f26d6dae6251e3a584f922abfb0dde56
theblessed-coder <blessingnwakwuo8@gmail.com>
2023-09-15 19:07:20 +0100

    added hover on submit button

--- frontend/src/pages/SignupPage.js
+++ frontend/src/pages/SignupPage.js
@@ -16,10 +16,10 @@ const SignupPage = () => {
 
 
     return (
-      <div className="max-w-md mx-auto bg-white rounded p-8 shadow-lg">
+      <div className="max-w-lg mx-auto my-5 bg-white rounded p-8 shadow-lg flex flex-col">
         <form onSubmit={handleSubmit}>
           <div className="mb-4">
-            <label className="block font-semibold mb-2">First Name:</label>
+            <label className="block font-semibold mb-2">First Name</label>
             <input
               type="text"
               name="firstName"
@@ -31,7 +31,7 @@ const SignupPage = () => {
           </div>
 
           <div className="mb-4">
-            <label className="block font-semibold mb-2">Last Name:</label>
+            <label className="block font-semibold mb-2">Last Name</label>
             <input
               type="text"
               name="lastName"
@@ -43,7 +43,7 @@ const SignupPage = () => {
           </div>
 
           <div className="mb-4">
-            <label className="block font-semibold mb-2">Email:</label>
+            <label className="block font-semibold mb-2">Email</label>
             <input
               type="email"
               name="email"
@@ -55,7 +55,7 @@ const SignupPage = () => {
           </div>
 
           <div className="mb-6">
-            <label className="block font-semibold mb-2">Password:</label>
+            <label className="block font-semibold mb-2">Password</label>
             <input
               type="password"
               name="password"
@@ -71,7 +71,7 @@ const SignupPage = () => {
           </p>
           <button
             type="submit"
-            className="bg-yellow-500 text-black font-semibold w-full p-2 rounded"
+            className="bg-yellow-500 hover:bg-slate-500 hover:text-yellow-50 text-black font-semibold w-full p-2 rounded"
           >
             REGISTER NOW
           </button>
