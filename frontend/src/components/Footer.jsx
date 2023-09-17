191580c55c026b700a676634d4dbc20691c774e0
theblessed-coder <blessingnwakwuo8@gmail.com>
2023-09-17 02:00:57 +0100

    added styles

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -4,23 +4,26 @@ function Footer() {
   return (
     <div>
       <footer className="bg-black text-white text-center p-4 mt-auto">
-        <div className="flex justify-evenly">
-        <div>
-          <h1>Quick Links</h1>
-          <div className="flex flex-col p-4">
-            <a href="/" className="text-white hover:text-yellow-500 p-1">
-              Home
-            </a>
-            <a href="/about" className="text-white hover:text-yellow-500 p-1">
-              About Us
-            </a>
-            <a href="/contact" className="text-white hover:text-yellow-500 p-1">
-              Contact
-            </a>
-          </div>
+        <div className="flex justify-evenly pt-10">
+          <div>
+            <h1 className="text-xl font-bold">Quick Links</h1>
+            <div className="flex flex-col p-4">
+              <a href="/" className="text-white hover:text-yellow-500 p-1">
+                Home
+              </a>
+              <a href="/about" className="text-white hover:text-yellow-500 p-1">
+                About Us
+              </a>
+              <a
+                href="/contact"
+                className="text-white hover:text-yellow-500 p-1"
+              >
+                Contact
+              </a>
+            </div>
           </div>
           <div>
-            <h1>Follow Us</h1>
+            <h1 className="text-xl font-bold">Follow Us</h1>
             <div className="flex justify-center mt-4 space-x-4">
               <a
                 href="https://www.instagram.com/"
@@ -47,7 +50,7 @@ function Footer() {
           </div>
         </div>
         <hr />
-        <p>CasaQuestHub &copy; 2023. All rights reserved.</p>
+        <p className="pt-3">CasaQuestHub &copy; 2023. All rights reserved.</p>
       </footer>
     </div>
   );
