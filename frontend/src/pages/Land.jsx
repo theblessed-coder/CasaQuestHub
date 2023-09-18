5d9c0671df6d94667e6d155813c57e7c435da6c2
theblessed-coder <blessingnwakwuo8@gmail.com>
2023-09-18 16:47:30 +0100

    added initial data for land listings in the array

--- frontend/src/pages/Land.jsx
+++ frontend/src/pages/Land.jsx
@@ -7,6 +7,98 @@ const Land = () => {
       setSearchQuery(e.target.value);
     };
 
+     const landListings = [
+       {
+         id: 1,
+         title: "",
+         location: "",
+         price: "",
+         description: "",
+         imageUrl: "",
+       },
+
+       {
+         id: 2,
+         title: "",
+         location: "",
+         price: "",
+         description: "",
+         imageUrl: "",
+       },
+
+       {
+         id: 3,
+         title: "",
+         location: "",
+         price: "",
+         description: "",
+         imageUrl: "",
+       },
+
+       {
+         id: 4,
+         title: "",
+         location: "",
+         price: "",
+         description: "",
+         imageUrl: "",
+       },
+
+       {
+         id: 5,
+         title: "",
+         location: "",
+         price: "",
+         description: "",
+         imageUrl: "",
+       },
+
+       {
+         id: 6,
+         title: "",
+         location: "",
+         price: "",
+         description: "",
+         imageUrl: "",
+       },
+
+       {
+         id: 7,
+         title: "",
+         location: "",
+         price: "",
+         description: "",
+         imageUrl: "",
+       },
+
+       {
+         id: 8,
+         title: "",
+         location: "",
+         price: "",
+         description: "",
+         imageUrl: "",
+       },
+
+       {
+         id: 9,
+         title: "",
+         location: "",
+         price: "",
+         description: "",
+         imageUrl: "",
+       },
+
+       {
+         id: 10,
+         title: "",
+         location: "",
+         price: "",
+         description: "",
+         imageUrl: "",
+       },
+     ];
+    
   return (
     <div>Land</div>
   )
