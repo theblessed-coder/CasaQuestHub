ad028e556f29b50e51a64eed673920e3d5c732fd
theblessed-coder <blessingnwakwuo8@gmail.com>
2023-09-18 02:37:54 +0100

    added residential listings

---
+++ frontend/src/pages/Residential.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const Residential = () => {
+    const ResidentialListings = [
+      {
+        id: 1,
+        title: "Beautiful Family Home",
+        location: "Victoria Island, Lagos",
+        price: "$300,000",
+        description:
+          "A spacious family home with a large backyard and modern amenities.",
+        imageUrl: "",
+      },
+
+      {
+        id: 2,
+        title: "Cozy Townhouse",
+        location: "Ada george, Rivers",
+        price: "$200,000",
+        description: "A charming townhouse with easy access to local amenities.",
+        imageUrl: "/images/residential2.jpg",
+      },
+    ];
+
+  return (
+    <div>Residential</div>
+  )
+}
+
+export default Residential
