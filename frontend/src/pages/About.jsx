d0d048afa9fb0bd0dffb34f0be1a42f6ad92df2b
theblessed-coder <blessingnwakwuo8@gmail.com>
2023-09-19 17:13:23 +0100

    Added UI elements for rating selection and submit button in the review form

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-import Reviews from "./components/Reviews";
+import Reviews from "../components/Reviews";
 
 const About = () => {
   return (
-    <div className="max-w-4xl mx-auto p-4 h-screen">
+    <div className="max-w-4xl mx-auto ">
       <h1 className="text-3xl font-bold mb-4">About CasaQuestHub</h1>
       <p className="mb-4">
         Casa Quest Hub is a user-friendly and
