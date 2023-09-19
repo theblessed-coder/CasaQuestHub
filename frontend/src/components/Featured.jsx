cf18ad7105cfe0afae2f837cdb3204d039e1b024
theblessed-coder <blessingnwakwuo8@gmail.com>
2023-09-19 17:43:24 +0100

    Added JSX elements for displaying featured listing details, including image, title, location, price, description, and buttons for 'View Details' and 'Checkout'

--- frontend/src/components/Featured.jsx
+++ frontend/src/components/Featured.jsx
@@ -36,7 +36,25 @@ const Featured = () => {
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
       {featuredListings.map((listing) => (
-        <div key={listing.id}>{/* Display the featured listing */}</div>
+        <div key={listing.id}>
+          <img
+            src={listing.imageUrl}
+            alt={listing.title}
+            className="w-full h-48 object-cover mb-4"
+          />
+          <h2 className="text-xl font-bold mb-2">{listing.title}</h2>
+          <p className="text-gray-700 mb-2">{listing.location}</p>
+          <p className="text-green-600 font-semibold mb-2">{listing.price}</p>
+          <p className="text-gray-800">{listing.description}</p>
+          <div className="mt-4">
+            <button className="bg-blue-500 text-white font-semibold py-2 px-4 rounded hover:bg-blue-700 mr-2">
+              View Details
+            </button>
+            <button className="bg-green-500 text-white font-semibold py-2 px-4 rounded hover:bg-green-700">
+              Checkout
+            </button>
+          </div>
+        </div>
       ))}
     </div>
   );
