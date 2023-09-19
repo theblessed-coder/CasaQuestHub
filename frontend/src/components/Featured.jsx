5c5cde01b9142c64976e031cd5a0eac5a2d453d8
theblessed-coder <blessingnwakwuo8@gmail.com>
2023-09-19 17:38:56 +0100

    Added data in the featured listing array

--- frontend/src/components/Featured.jsx
+++ frontend/src/components/Featured.jsx
@@ -1,8 +1,36 @@
 import React from "react";
 
-const FeaturedBuildings = () => {
+const Featured = () => {
   const featuredListings = [
-    // Add your featured listings here
+    {
+      id: 1,
+      title: "Tranquil Estate in Port Harcourt",
+      location: "GRA Phase 3, Port Harcourt",
+      price: "2,000,000.00",
+      description:
+        "Experience sophistication in this sleek penthouse, featuring high-end finishes and panoramic city views.",
+      imageUrl: "./images/residential-03.jpg",
+    },
+
+    {
+      id: 2,
+      title: "Commercial Complex",
+      location: "Abuja Central Business District",
+      price: "150,000.00 per month",
+      description:
+        "A well-maintained commercial plaza with ample parking space.",
+      imageUrl: "./images/commercial-03.jpg",
+    },
+
+    {
+      id: 5,
+      title: "Industrial Land",
+      location: "Port Harcourt, Rivers",
+      price: "3,800,000.00",
+      description:
+        "A suitable land for industrial purposes with good access to transportation.",
+      imageUrl: "./images/land-05.jpg",
+    },
   ];
 
   return (
@@ -14,4 +42,4 @@ const FeaturedBuildings = () => {
   );
 };
 
-export default FeaturedBuildings;
+export default Featured;
