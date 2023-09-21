08cf4b0021447a73d94ba7c448ad52aaa6a2c8fd
Ashaolu Peter <pyrotech.sv@gmail.com>
2023-09-21 13:50:10 +0100

    database

--- Backend/config/database.js
+++ Backend/config/database.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 // Definining  the MongoDB connection URI with actual database URI.
-const mongoURI = 'mongodb+srv://itodoemmanuelitodo:<password>@cluster0.71nirhd.mongodb.net/?retryWrites=true&w=majority'
+const mongoURI = 'mongodb+srv://itodoemmanuelitodo:<Velocity400>@cluster0.71nirhd.mongodb.net/?retryWrites=true&w=majority'
 // Connect to the MongoDB database
 mongoose.connect(mongoURI, {
   useNewUrlParser: true,
