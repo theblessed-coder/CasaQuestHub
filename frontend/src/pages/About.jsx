9b28d62809a6520f16ad7a4871f32d7c7fa8cdd7
Moses-Onche <onchemoses38@gmail.com>
2024-11-15 21:46:19 +0100

    Add shadow to Share a Review section. Adjust margin bottom and padding of box content

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -4,15 +4,16 @@ import Reviews from "../components/Reviews";
 const About = () => {
   return (
     <div className="max-w-4xl mx-auto ">
-      <h1 className="text-3xl font-bold mt-6 mb-4">About CasaQuestHub</h1>
-      <p className="mb-4">
+      <h1 className="text-3xl font-bold mt-6 mb-2 px-6">About CasaQuestHub</h1>
+      <hr className="mb-4 mx-6"/>
+      <p className="mb-4 px-6">
         Casa Quest Hub is a user-friendly and
         feature-rich real estate website aimed at simplifying the process of
         buying, selling, and renting properties. We offer a comprehensive
         database of property listings, allowing users to search based on various
         criteria such as location, price, property type, and more.
       </p>
-      <p className="mb-4">
+      <p className="mb-4 px-6">
         It will also incorporate interactive maps and contact information to
         facilitate communication between buyers, sellers, and real estate
         agents. Additionally, the website will prioritize responsive design to
